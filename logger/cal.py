4295f39abc805beb0ec536c2f6b59bfa36dca494
shivamjos1994 <shivamjos1994@gmail.com>
2023-11-02 17:36:30 +0530

    connect the logged data with aws app insights

--- logger/cal.py
+++ logger/cal.py
@@ -1,40 +1,62 @@
 import logging
 import datetime
+# provides an interface for interacting with AWS services
+import boto3
+import time
 
-from opentelemetry.sdk._logs import (
-    LoggerProvider,
-    LoggingHandler,
-    set_logger_provider,
-)
-#  to send logging related telemetry to Azure Monitor.
-from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter
+# configure the aws region (Mumbai)
+aws_region = "ap-south-1"
 
-# collects log records in a buffer and exports them periodically or when the buffer is full.
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-
-# for creating and managing Logger instances. 
-logger_provider = LoggerProvider()
-set_logger_provider(logger_provider)
-
-exporter = AzureMonitorLogExporter()
-
-logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
-
-# LoggingHandler class forwards log records from the logging module to the OpenTelemetry logger provider.
-handler = LoggingHandler()
 
 fmt = '%Y-%m-%d %H:%M:%S'
 
+# creating a cloudwatch to log client
+#  client object provides methods for interacting with the CloudWatch Logs service, such as creating log groups and streams, putting log events, and querying log data.
+cloudwatch = boto3.client("logs", region_name = aws_region)
+
+# creating cloudwatch log group an stream if they don't already exist.
+
+#  A log group is a collection of log streams that have the same retention, monitoring, and access control settings. 
+log_group_name = "Calculation-app"
+
+# A log stream is a sequence of log events that share the same source.
+log_stream_name = "Calculation-stream"
+
+try:
+    cloudwatch.create_log_group(logGroupName = log_group_name)
+except cloudwatch.exceptions.ResourceAlreadyExistsException:
+    pass
+
+try:
+    cloudwatch.create_log_stream(logGroupName = log_group_name, logStreamName = log_stream_name)
+except cloudwatch.exceptions.ResourceAlreadyExistsException:
+    pass
+
 logger = logging.getLogger(__name__)
 
+formatter = logging.Formatter('%(levelname)s: %(name)s: %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
 
-# formatter = logging.Formatter('%(levelname)s: %(name)s: %(message)s')
-# stream_handler = logging.StreamHandler()
-# stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
 
-# attaching LoggingHandler to root logger.
-logger.addHandler(handler)
-logger.setLevel(logging.NOTSET)
+
+# function to take log messages and log level.
+def send_log_to_cloudWatch(log_message, log_level):
+    log_level = log_level.upper()
+    # uploads a batch of log events to the specified log stream in CloudWatch Logs.
+    response = cloudwatch.put_log_events(
+        logGroupName = log_group_name,
+        logStreamName = log_stream_name,
+        logEvents = [
+            {
+               'timestamp': int(time.time() * 1000),
+               'message': f"[{log_level}] {log_message}"
+            }
+        ]
+    )
+    return response
 
 
 def calculate_result(operator):
@@ -53,16 +75,20 @@ def calculate_result(operator):
                 result = num1 / num2
                 operation = "Division"
             except ZeroDivisionError:
-                logger.error("Can not divide by Zero")
+                error_message = "Can not divide by Zero"
+                send_log_to_cloudWatch(error_message, "error")
                 return
         elif operator == "%":
             result = num1 % num2
             operation = "Modulus"
     else:
-        logger.error("Invalid operator")
+        error_message = "Invalid operator"
+        send_log_to_cloudWatch(error_message, "error")
         return
-    logger.info(f"Result of {operation}: {result} at {datetime.datetime.now().strftime(fmt)}")
-    logger.debug(f"{operation} operation performed")
+    info_message = f"Result of {operation}: {result} at {datetime.datetime.now().strftime(fmt)}"
+    send_log_to_cloudWatch(info_message, "info")
+    debug_message = f"{operation} operation performed"
+    send_log_to_cloudWatch(debug_message, "debug")
 
 
 operators = {
@@ -75,21 +101,28 @@ operators = {
 
 try:
     num1 = float(input("Enter the first number: "))
-    logger.debug("User entered the first number")
-    logger.info(f"{num1} pressed at {datetime.datetime.now().strftime(fmt)}")
+    debug_message = "User entered the first number"
+    info_message = f"{num1} pressed at {datetime.datetime.now().strftime(fmt)}"
+    send_log_to_cloudWatch(info_message, "info")
+    send_log_to_cloudWatch(debug_message, "debug")
 
     num2 = float(input("Enter the second number: "))
-    logger.debug("User entered the second number ")
-    logger.info(f"{num2} pressed at {datetime.datetime.now().strftime(fmt)}")
+    debug_message = "User entered the second number "
+    info_message = f"{num2} pressed at {datetime.datetime.now().strftime(fmt)}"
+    send_log_to_cloudWatch(info_message, "info")
+    send_log_to_cloudWatch(debug_message, "debug")
 
     operator = input("Enter an operator ( +, -, *, /, % ): ")
 
     if operator in operators:
-        logger.info(f"{operator} pressed at {datetime.datetime.now().strftime(fmt)}")
+        info_message = f"{operator} pressed at {datetime.datetime.now().strftime(fmt)}"
+        send_log_to_cloudWatch(info_message, "info")
         calculate_result(operator)
     else:
-        logger.error("Invalid operator")
+         error_message = "Invalid operator"
+         send_log_to_cloudWatch(error_message, "error")
 
 except ValueError:
-    logger.error("Invalid input, enter the correct number")
+     error_message = "Invalid input, enter the correct number"
+     send_log_to_cloudWatch(error_message, "error")
 
